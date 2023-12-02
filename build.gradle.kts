a3d369b0491835c3490971598454bc287d30c945
Alessandro Ciccimarra <alessandro.ciccimarra@gmail.com>
2023-12-02 08:25:55 +0000

    Add test config

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,17 @@ plugins {
     kotlin("jvm") version "1.9.21"
 }
 
-sourceSets {
-    main {
-        kotlin.srcDir("src")
-    }
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks {
     wrapper {
         gradleVersion = "8.5"
     }
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
 }
---
+++ src/test/kotlin/Day01Test.kt
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test
+
+class Day01Test {
+    @Test
+    fun `test`() {
+        println("")
+    }
+}
