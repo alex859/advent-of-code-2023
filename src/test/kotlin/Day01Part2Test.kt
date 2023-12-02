b2917821f6d5fb95f105bb9e8a254b8d1054604e
Alessandro Ciccimarra <alessandro.ciccimarra@gmail.com>
2023-12-02 11:42:13 +0000

    unused import

--- src/test/kotlin/Day01Part2Test.kt
+++ src/test/kotlin/Day01Part2Test.kt
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.DynamicTest.dynamicTest
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestFactory
